e5fbb37623bfd762fde98e85f9f78e0dd366ea6f
Lina Borah <26639248+LinaBorah@users.noreply.github.com>
2023-06-16 15:21:05 +0530

    Set up the semantic structure of the website

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <img src="src\assets\Logo .svg"></img>
+            
+        </footer>
+    );
+}
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <header>
+            <img src="src\assets\Logo .svg"></img>
+        </header>
+    );
+}
+export default Header;
